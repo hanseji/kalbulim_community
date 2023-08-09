46cd5c1a93d0d6c78d9b0002ca7d3fae26b1d5b9
16st58 <105636718+16st58@users.noreply.github.com>
2023-08-10 00:51:50 +0900

    위치 정보 에러 없애기

--- script.js
+++ script.js
@@ -101,19 +101,10 @@ function locationLoadSuccess(pos){
 
   // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
   map.panTo(currentPos);
-
-  // 마커 생성
-  var marker = new kakao.maps.Marker({
-      position: currentPos
-  });
-
-  // 기존에 마커가 있다면 제거
-  marker.setMap(null);
-  marker.setMap(map);
 };
 
 function locationLoadError(pos){
-  alert('위치 정보를 가져오는데 실패했습니다.');
+  //alert('위치 정보를 가져오는데 실패했습니다.');
 };
 
 // 위치 가져오기 버튼 클릭시
