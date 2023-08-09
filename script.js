56e5c0a9dfa724c2dff346f48981b910b92a9811
16st58 <105636718+16st58@users.noreply.github.com>
2023-08-10 00:51:40 +0900

    위치 정보 가져오기

--- script.js
+++ script.js
@@ -89,34 +89,39 @@ function upload() {
 
 var locPosition = new kakao.maps.LatLng(37.4812845080678, 126.952713197762)
 
-//map.html
-function currentLocation() {
-  // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
-  if (navigator.geolocation) {
-
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function (position) {
-
-      var lat = position.coords.latitude, // 위도
-        lon = position.coords.longitude; // 경도
-
-      locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-
-    });
-  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-
-    locPosition = new kakao.maps.LatLng(37.4812845080678, 126.952713197762)
-  }
-  return true
-}
-
-currentLocation();
-
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
   center: locPosition, //지도의 중심좌표.
   level: 10 //지도의 레벨(확대, 축소 정도)
 };
+
+function locationLoadSuccess(pos){
+  // 현재 위치 받아오기
+  var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+
+  // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+  map.panTo(currentPos);
+
+  // 마커 생성
+  var marker = new kakao.maps.Marker({
+      position: currentPos
+  });
+
+  // 기존에 마커가 있다면 제거
+  marker.setMap(null);
+  marker.setMap(map);
+};
+
+function locationLoadError(pos){
+  alert('위치 정보를 가져오는데 실패했습니다.');
+};
+
+// 위치 가져오기 버튼 클릭시
+function getCurrentPosBtn(){
+  navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
+};
+
+getCurrentPosBtn();
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
